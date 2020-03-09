30e94100f2e5a1a97a04146bbd30927540f79edc
sahid rojas <sahid.rojas64@gmail.com>
2020-03-08 18:33:58 -0600

    Creates the GET method for the data of the packages

--- TECBox_Backend/TECBox_Api/offices.cs
+++ TECBox_Backend/TECBox_Api/offices.cs
@@ -1,7 +1,7 @@
 namespace offices
 {
     public class offices
-    {
+    {        
          public string name { get; set; }
         public string province { get; set; }
         public string distrit{get;set;}
---
+++ TECBox_Backend/TECBox_Api/packages.cs
@@ -0,0 +1,12 @@
+namespace packages
+{
+    public class packages
+    {
+        public string id { get; set; }
+        public string client { get; set; }
+        public string description{get;set;}
+        public string arivalDate{get;set;}
+
+    }
+    
+}
