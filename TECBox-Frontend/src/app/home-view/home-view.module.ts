95163c10682118f493e767b1ec1c545710244901
Jessica Espinoza <jessicaes2000@gmail.com>
2020-03-15 04:19:16 -0600

    [UPDATE] Added products on home-view

--- TECBox-Frontend/src/app/home-view/home-view.module.ts
+++ TECBox-Frontend/src/app/home-view/home-view.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { HomeViewRoutingModule } from './home-view-routing.module';
 import { HomeViewComponent } from './home-view.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// search module
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 
 @NgModule({
   declarations: [HomeViewComponent],
   imports: [
     CommonModule,
-    HomeViewRoutingModule
+    HomeViewRoutingModule,
+    ReactiveFormsModule,
+    Ng2SearchPipeModule,
+    FormsModule
   ]
 })
 export class HomeViewModule { }
