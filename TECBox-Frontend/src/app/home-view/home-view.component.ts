858e14d1cce7e293c2ea501c755e809a1543372e
Jessica Espinoza <jessicaes2000@gmail.com>
2020-03-15 14:06:44 -0600

    [ADD] Added modal

--- TECBox-Frontend/src/app/home-view/home-view.component.ts
+++ TECBox-Frontend/src/app/home-view/home-view.component.ts
@@ -48,6 +48,66 @@ export class HomeViewComponent implements OnInit {
       "impuesto": "0.13",
       "descuento": "0.1",
       "imagen" : "https://www.decowoerner.com/images/contentUploads/pictures/500_599/550/www/101000/550_116_00-1-0-00.jpg"
+    },
+    {
+      "nombre": "cuerda",
+      "descripcion": "atar cosas",
+      "codigo_barras": "123",
+      "vendedor": "panchito",
+      "precio_compra": "20",
+      "impuesto": "0.7",
+      "descuento": "0",
+      "imagen" : "https://www.decowoerner.com/images/contentUploads/pictures/500_599/550/www/101000/550_116_00-1-0-00.jpg"
+    },
+    {
+      "nombre": "mechero",
+      "descripcion": "quemar infieles",
+      "codigo_barras": "124",
+      "vendedor": "panchito",
+      "precio_compra": "10",
+      "impuesto": "0.4",
+      "descuento": "0.2",
+      "imagen" : "https://images-na.ssl-images-amazon.com/images/I/41tTOKXoUHL._SX342_.jpg"
+    },
+    {
+      "nombre": "cuerda",
+      "descripcion": "zapatos",
+      "codigo_barras": "125",
+      "vendedor": "no se",
+      "precio_compra": "100",
+      "impuesto": "0.13",
+      "descuento": "0.1",
+      "imagen" : "https://www.decowoerner.com/images/contentUploads/pictures/500_599/550/www/101000/550_116_00-1-0-00.jpg"
+    },
+    {
+      "nombre": "cuerda",
+      "descripcion": "atar cosas",
+      "codigo_barras": "123",
+      "vendedor": "panchito",
+      "precio_compra": "20",
+      "impuesto": "0.7",
+      "descuento": "0",
+      "imagen" : "https://www.decowoerner.com/images/contentUploads/pictures/500_599/550/www/101000/550_116_00-1-0-00.jpg"
+    },
+    {
+      "nombre": "mechero",
+      "descripcion": "quemar infieles",
+      "codigo_barras": "124",
+      "vendedor": "panchito",
+      "precio_compra": "10",
+      "impuesto": "0.4",
+      "descuento": "0.2",
+      "imagen" : "https://images-na.ssl-images-amazon.com/images/I/41tTOKXoUHL._SX342_.jpg"
+    },
+    {
+      "nombre": "cuerda",
+      "descripcion": "zapatos",
+      "codigo_barras": "125",
+      "vendedor": "no se",
+      "precio_compra": "100",
+      "impuesto": "0.13",
+      "descuento": "0.1",
+      "imagen" : "https://www.decowoerner.com/images/contentUploads/pictures/500_599/550/www/101000/550_116_00-1-0-00.jpg"
     }
   ];
 
@@ -65,7 +125,6 @@ export class HomeViewComponent implements OnInit {
   }
 
   calcPurchaseTotal(){
-    
   }
 
 }
