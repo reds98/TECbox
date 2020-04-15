64a09da4a91a9f6b61dbca8d15f90b3457790708
Jessica Espinoza <jessicaes2000@gmail.com>
2020-04-15 13:14:29 -0600

    [ADD] Added consulting package in cellar view

--- TECBox-Frontend/src/app/cellar-view/cellar-view.component.ts
+++ TECBox-Frontend/src/app/cellar-view/cellar-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-cellar-view',
@@ -12,31 +12,76 @@ export class CellarViewComponent implements OnInit {
   constructor(private httpService: HttpClient) { }
 
   columnsParent = ["ID", "Cliente", "Descripción", "Fecha de Entrega"];
+  consultationRequest = false;
+  isNumber = true;
+  state;
+  userForm = new FormGroup({
+    TrackingID: new FormControl()
+    });
+
+  objectValues = Object.values;
+  objectKeys = Object.keys;
 
   dataParent = [
     {
-      "ID": "001",
-      "Cliente": "Homero",
-      "Descripción": "Feo",
-      "Fecha de Entrega": "Hoy"
-    },
-    {    
-      "ID": "002",
-      "Cliente": "Maggie",
-      "Descripción": "Bebe",
-      "Fecha de Entrega": "Dos meses"
-    },
-    {
-      "ID": "003",
-      "Cliente": "Bart",
-      "Descripción": "Cool",
-      "Fecha de Entrega": "Ayer"
-    }
-  ];
+    "ID": "001",
+    "Client": "Homero",
+    "Description": "Feo",
+    "ArivalDate": "Hoy",
+    "State" : "Listo para entrega",
+    "District" : "UwU",
+    "Route" : "[OwO, UwU, opo]",
+    "Discount" : "null",
+    "Tax" : "0"
+  },
+  {    
+    "ID": "002",
+    "Client": "Maggie",
+    "Description": "Bebe",
+    "ArivalDate": "Dos meses",
+    "State":"En sucursa",
+    "District" : "OwO",
+    "Route" : "[OwO, Uwu, opo]",
+    "Discount" : "null",
+    "Tax" : "0"
+  },
+  {
+    "ID": "003",
+    "Client": "Bart",
+    "Description": "Cool",
+    "ArivalDate": "Ayer",
+    "State":"En ruta de entrega",
+    "District" : "OwO",
+    "Route" : "[OwO, Uwu, opo]",
+    "Discount" : "null",
+    "Tax" : "0"
+  }
+];
+
   
   ngOnInit(): void {
 
   }
 
+  isConsulting(){
+    this.consultationRequest = true;
+  }
 
+  consultPackage(){
+    var value = this.userForm.get('TrackingID').value;
+    if(!isNaN(value)){
+      this.isNumber = true;
+
+      // Arreglar con un for (voy a mimis)
+      if(value == this.objectValues(this.dataParent[0])[0]){
+        this.state = this.objectValues(this.dataParent[0])[4];
+        console.log(this.objectValues(this.dataParent[0])[4]);
+      }
+
+    }
+    else{
+      this.isNumber = false;
+    }
+  }
+  
 }
