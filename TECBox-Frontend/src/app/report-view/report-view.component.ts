fabe976b513a8a3d0195e031c1ead2eb0e59f464
Jessica Espinoza <jessicaes2000@gmail.com>
2020-05-16 23:12:37 -0600

    [UPDATE] Fixed bug

--- TECBox-Frontend/src/app/report-view/report-view.component.ts
+++ TECBox-Frontend/src/app/report-view/report-view.component.ts
@@ -11,8 +11,15 @@ export class ReportViewComponent implements OnInit {
   startDate = new Date(2000, 0, 1);
   finalDate = new Date(2020, 0, 1);
 
+  startDateInput = this.startDate;
+  endDateInput = this.finalDate;
+
   ngOnInit(): void {
   }
 
+
+  print(prod1, prod2){
+    console.log(prod1, prod2);
+  }
 }
 
