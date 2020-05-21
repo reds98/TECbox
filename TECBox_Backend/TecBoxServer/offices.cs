a2939c713a7f9abcbb4deedded5dcd4de7aac134
Jessica Espinoza <jessicaes2000@gmail.com>
2020-05-21 02:03:39 -0600

    [ADD] Added server files to git

---
+++ TECBox_Backend/TecBoxServer/offices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TecBoxServer
+{
+    public class offices
+    {
+        public string name { get; set; }
+        public string province { get; set; }
+        public string distrit { get; set; }
+        public string phone_number { get; set; }
+        public string manager { get; set; }
+    }
+}
